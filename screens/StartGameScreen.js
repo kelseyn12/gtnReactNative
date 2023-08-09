15087ae4645bb66ca4df8f5d87866ad922d4cc7c
kelseyn12 <kelseynocek12@gmail.com>
2023-08-09 13:32:38 -0500

    added my own buttons by creating a button component and using it in startgamescreen component. Also learned to style them

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,13 +1,30 @@
 //This will hold component that will act as start screen where we pick the number that should be guessed and where we start the game
 
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen(){
-    return 
-    <View>
+    return (
+    <View style={styles.inputContainer}>
         <TextInput />
-        //BUTTONS
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
     </View>
-}
+)}
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: '#E6E6FA',
+        borderRadius: 8,
+        elevation: 4, //android shadow
+        shadowColor: 'black', //ios shadow
+        shadowOffset: { width: 0, height: 2 }, //ios shadow
+        shadowRadius: 6, //ios shadow
+        shadowOpacity: 0.25, //ios shadow
+    }
+})
