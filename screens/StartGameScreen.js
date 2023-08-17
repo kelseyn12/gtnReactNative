8c36c143c3ebe346f4519cc2b6268a3da04a542a
kelseyn12 <kelseynocek12@gmail.com>
2023-08-17 16:36:59 -0500

    styled buttons a little better on start game screen component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,39 +3,60 @@
 import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen(){
-    return (
+function StartGameScreen() {
+  return (
     <View style={styles.inputContainer}>
-        <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
-)}
+  );
+}
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: '#C2BBF0',
-        borderRadius: 8,
-        elevation: 4, //android shadow
-        shadowColor: 'black', //ios shadow
-        shadowOffset: { width: 0, height: 2 }, //ios shadow
-        shadowRadius: 6, //ios shadow
-        shadowOpacity: 0.25, //ios shadow
-    },
-    numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: '#FFFDD0',
-        borderBottomWidth: 2,
-        color: '#FFFDD0',
-        marginVertical: 8,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    }
-})
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#C2BBF0",
+    borderRadius: 8,
+    elevation: 4, //android shadow
+    shadowColor: "black", //ios shadow
+    shadowOffset: { width: 0, height: 2 }, //ios shadow
+    shadowRadius: 6, //ios shadow
+    shadowOpacity: 0.25, //ios shadow
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#FFFDD0",
+    borderBottomWidth: 2,
+    color: "#FFFDD0",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
