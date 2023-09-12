e356ef2a67ab479efe749fe59f049309c51989c2
kelseyn12 <kelseynocek12@gmail.com>
2023-09-12 13:18:59 -0500

    added logic to check if number is valid and if it is, render the game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,1 +1,9 @@
 //Will be responsible for actual game- guesses by phone and where we let phone know if guess is too high or low
+
+import { Text } from 'react-native';
+
+function GameScreen () {
+    return <Text>Game Screen</Text>
+}
+
+export default GameScreen;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   //react native will invoke this function for every keystroke on the text input
@@ -30,7 +30,7 @@ function StartGameScreen() {
       return;
     }
 
-    console.log("Valid number!");
+    onPickNumber(chosenNumber);
   }
 
   return (
