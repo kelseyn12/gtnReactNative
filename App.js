34fff8a8bf5a6b4758a82475ba63232e43a365df
kelseyn12 <kelseynocek12@gmail.com>
2023-09-12 10:44:21 -0500

    added background image and gave it an opacity

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import StartGameScreen from './screens/StartGameScreen';
+import { StyleSheet, ImageBackground } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <LinearGradient colors={['#C2BBF0','#ddb52f']} style={styles.rootScreen}>
-    <StartGameScreen />
+    <LinearGradient colors={["#C2BBF0", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   }
 });
