19dea5a2296aae7789a701afb8e7c485c002d066
kelseyn12 <kelseynocek12@gmail.com>
2023-09-25 16:27:41 -0500

    downloaded app loading to use to laod while loading fonts

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from './screens/GameScreen';
