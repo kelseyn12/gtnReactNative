7b4c72c7faac0e2bbdc6718cb5323627108ff9d2
kelseyn12 <kelseynocek12@gmail.com>
2023-09-25 16:58:53 -0500

    added an image to put in game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,43 @@
 //will be shown once game is over
-import { Text } from 'react-native';
+import { View, Image, StyleSheet, Text } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
-function GameOverScreen () {
-    return (
-        <Text>Game is Over</Text>
-    )
+function GameOverScreen() {
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text> Your phone needed X rounds to guess the number Y.</Text>
+    </View>
+  );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary600,
+    overflow: "hidden",
+    mergin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
