32746ad13277bfe136df7e39d2af99d81934121a
kelseyn12 <kelseynocek12@gmail.com>
2023-09-26 12:34:10 -0500

    ls logged game rounds by mapping through prev guesses

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 //Will be responsible for actual game- guesses by phone and where we let phone know if guess is too high or low
 import { useEffect, useState } from "react";
-import { View, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert, Text } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 
 import Title from "../components/ui/Title";
@@ -25,6 +25,7 @@ let maxBoundary = 100;
 function GameScreen({ userNumber, onGameOver }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [guessRounds, setGuessRounds] = useState([initialGuess]);
 
   useEffect(() => {
     if (currentGuess === userNumber) {
@@ -59,6 +60,7 @@ function GameScreen({ userNumber, onGameOver }) {
       currentGuess
     );
     setCurrentGuess(newRndNumber);
+    setGuessRounds(prevGuessRounds => [newRndNumber, ...prevGuessRounds]);
   }
 
   return (
@@ -80,7 +82,9 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      {/* LOG ROUNDS */}
+      <View>
+        {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)}
+      </View>
     </View>
   );
 }
