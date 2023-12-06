4298edef1703ec8d688826bf4e92e75e5806293d
kelseyn12 <kelseynocek12@gmail.com>
2023-12-06 13:14:50 -0600

    added platform specific files for colors too

---
+++ constants/colors.ios.js
@@ -0,0 +1,10 @@
+const Colors = {
+    primary400: "#9D91E7",
+    primary500: "#7867DE",
+    primary600: "#432DCB",
+    accent500: "#FFFB9D",
+    accent800: "#BB971E",
+    plain: "white",
+}
+
+export default Colors;
